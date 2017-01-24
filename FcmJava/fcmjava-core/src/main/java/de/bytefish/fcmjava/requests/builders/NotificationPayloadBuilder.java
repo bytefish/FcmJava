ca1790a2ec01d78e323ec48ca3bfb32514060562
Francisco Aranda <francisco.aranda@embedit.cz>
2017-01-24 13:21:01 +0100

    NotificationPayloadBuilder added

---
+++ FcmJava/fcmjava-core/src/main/java/de/bytefish/fcmjava/requests/builders/NotificationPayloadBuilder.java
@@ -0,0 +1,106 @@
+package de.bytefish.fcmjava.requests.builders;
+
+import de.bytefish.fcmjava.requests.notification.NotificationPayload;
+import java.util.List;
+
+/**
+ * Builder helper class to make easier to create {@link NotificationPayload} instances.
+ * <p>
+ * All fields are optional, and some of them are common for both Android and iOS and some
+ * of them are specific to Android ({@link #icon}, {@link #tag}, {@link #color})
+ * or specific to iOS ({@link #badge}).
+ *
+ * @author Francisco Aranda {@code <fran.culebras@gmail.com>}
+ */
+public class NotificationPayloadBuilder {
+
+    private String title;
+    private String body;
+    private String icon;
+    private String sound;
+    private String badge;
+    private String tag;
+    private String color;
+    private String clickAction;
+    private String bodyLocKey;
+    private List<String> bodyLocKeyArgs;
+    private String titleLocKey;
+    private List<String> titleLocKeyArgs;
+
+    public NotificationPayloadBuilder setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setIcon(String icon) {
+        this.icon = icon;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setSound(String sound) {
+        this.sound = sound;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setBadge(String badge) {
+        this.badge = badge;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setTag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setColor(String color) {
+        this.color = color;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setClickAction(String clickAction) {
+        this.clickAction = clickAction;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setBodyLocKey(String bodyLocKey) {
+        this.bodyLocKey = bodyLocKey;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setBodyLocKeyArgs(List<String> bodyLocKeyArgs) {
+        this.bodyLocKeyArgs = bodyLocKeyArgs;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setTitleLocKey(String titleLocKey) {
+        this.titleLocKey = titleLocKey;
+        return this;
+    }
+
+    public NotificationPayloadBuilder setTitleLocKeyArgs(List<String> titleLocKeyArgs) {
+        this.titleLocKeyArgs = titleLocKeyArgs;
+        return this;
+    }
+
+    public NotificationPayload build() {
+        return new NotificationPayload(
+                title,
+                body,
+                icon,
+                sound,
+                badge,
+                tag,
+                color,
+                clickAction,
+                bodyLocKey,
+                bodyLocKeyArgs,
+                titleLocKey,
+                titleLocKeyArgs);
+    }
+
+}
