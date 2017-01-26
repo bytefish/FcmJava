72ff2c11ff87d977e5b7b8a5deec09fdf5c34c79
Philipp Wagner <bytefish@gmx.de>
2017-01-26 18:39:40 +0100

    Issue #11 Extend Logging
    Add better logging to the LoggingResponseInterceptor, so the
    HttpResponse Body (with incoming JSON) is logged.

--- FcmJava/fcmjava-client/src/main/java/de/bytefish/fcmjava/client/interceptors/response/LoggingResponseInterceptor.java
+++ FcmJava/fcmjava-client/src/main/java/de/bytefish/fcmjava/client/interceptors/response/LoggingResponseInterceptor.java
@@ -3,10 +3,13 @@
 
 package de.bytefish.fcmjava.client.interceptors.response;
 
+import de.bytefish.fcmjava.client.utils.OutParameter;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +22,39 @@ public class LoggingResponseInterceptor implements HttpResponseInterceptor {
     @Override
     public void process(HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
         if(log.isDebugEnabled()) {
-            log.debug(httpResponse.toString());
+            // Try to get the Response as a String:
+            OutParameter<String> httpResponseString = new OutParameter<>();
+            if(tryGetResponseBody(httpResponse, httpResponseString)) {
+                log.debug(httpResponseString.get());
+            }
+        }
+    }
+
+    private boolean tryGetResponseBody(HttpResponse httpResponse, OutParameter<String> httpEntityString) {
+        // This shouldn't happen:
+        if(httpResponse == null) {
+            return false;
+        }
+        // Exit, if we don't have an Entity:
+        HttpEntity httpEntity = httpResponse.getEntity();
+        if(httpEntity == null) {
+            return false;
+        }
+
+        // Try to get the Entity as a String:
+        return tryGetEntityString(httpEntity, httpEntityString);
+    }
+
+    private boolean tryGetEntityString(HttpEntity httpEntity, OutParameter<String> httpEntityString) {
+        try {
+            httpEntityString.set(EntityUtils.toString(httpEntity));
+
+            return true;
+        } catch (Exception e) {
+            if(log.isErrorEnabled()) {
+                log.error("Failed to get the HttpEntity Content", e);
+            }
+            return false;
         }
     }
 }
