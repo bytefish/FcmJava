eaa247b42bc592e04292cef06777794375066858
Philipp Wagner <bytefish@gmx.de>
2017-04-30 10:39:48 +0200

    Issue #20 Add Unit Test for catching FcmException

--- FcmJava/fcmjava-client/src/test/java/de/bytefish/fcmjava/client/tests/integration/FcmClientIntegrationTest.java
+++ FcmJava/fcmjava-client/src/test/java/de/bytefish/fcmjava/client/tests/integration/FcmClientIntegrationTest.java
@@ -6,6 +6,9 @@ package de.bytefish.fcmjava.client.tests.integration;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import de.bytefish.fcmjava.client.FcmClient;
 import de.bytefish.fcmjava.client.settings.PropertiesBasedSettings;
+import de.bytefish.fcmjava.constants.Constants;
+import de.bytefish.fcmjava.exceptions.FcmAuthenticationException;
+import de.bytefish.fcmjava.http.options.IFcmClientSettings;
 import de.bytefish.fcmjava.model.options.FcmMessageOptions;
 import de.bytefish.fcmjava.model.topics.Topic;
 import de.bytefish.fcmjava.requests.data.DataMulticastMessage;
@@ -76,5 +79,45 @@ public class FcmClientIntegrationTest {
         FcmMessageResponse msgResponse = client.send(new DataMulticastMessage(options, registrationIds, new PersonData("Philipp", "Wagner")));
 
         Assert.assertNotNull(msgResponse);
+    }
+
+    @Test
+    @Ignore("This is an Integration Test using system properties to contact the FCM Server")
+    public void SendDataMulticastMessageWithExceptionTest() throws Exception {
+
+        // Create a Client with an Invalid API Key:
+        FcmClient client = new FcmClient(new IFcmClientSettings() {
+            @Override
+            public String getFcmUrl() {
+                return Constants.FCM_URL;
+            }
+
+            @Override
+            public String getApiKey() {
+                return "aa";
+            }
+        });
+
+        // Message Options:
+        FcmMessageOptions options = FcmMessageOptions.builder()
+                .setTimeToLive(Duration.ofHours(1))
+                .build();
+
+        ArrayList<String> registrationIds = new ArrayList<>();
+
+        registrationIds.add("invalid_key");
+
+        // We want to catch an FcmAuthenticationException:
+        boolean fcmAuthenticationExceptionThrown = false;
+
+        // Send the Data and catch the FcmAuthenticationException:
+        try {
+            client.send(new DataMulticastMessage(options, registrationIds, new PersonData("Philipp", "Wagner")));
+        } catch(FcmAuthenticationException e) {
+            fcmAuthenticationExceptionThrown = true;
+        }
+
+        // The Authentication Exception was caught:
+        Assert.assertEquals(true, fcmAuthenticationExceptionThrown);
     }
 }
